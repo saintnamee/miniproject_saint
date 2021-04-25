4f50cda837bbb166449c1a52256b3746376093a5
saintnamee <saintnamee@hotmail.com>
2021-04-26 01:04:38 +0700

    card

--- mini_saint/front/components/navbar.js
+++ mini_saint/front/components/navbar.js
@@ -1,18 +1,43 @@
 import Link from 'next/link'
 import styles from "../styles/Index.module.css";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
 
-const Navbar = () => (
-    <div>
-        <Link href="/"><a> <b>Home </b></a></Link> |
-        <Link href="/register"><a> <b>Register</b> </a></Link>  |
-        <Link href="/login"><a> <b>Login</b> </a></Link> |
-        <Link href="/profile"><a> <b>Profile</b> </a></Link> | 
-        <Link href="/foo"><a> <b>Foo </b></a></Link> |
-        <Link href="/studentedit"><a> <b>Student Edit</b> </a></Link> |
-        <Link href="/getConfig"><a><b> Config</b> </a></Link> | 
-        <Link href="/logout"><a> <b>Logout</b> </a></Link> 
-        
-    </div>
-)
+
+const Navbar = () => {
+    const classes = useStyles();
+    return(
+        <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    )
+}
+
 
 export default Navbar
