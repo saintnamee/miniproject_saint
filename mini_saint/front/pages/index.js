47bda1dfd4aa5dffee6172bf80fad7c4694ae00b
saintnamee <saintnamee@hotmail.com>
2021-04-27 22:24:40 +0700

    product

--- mini_saint/front/pages/index.js
+++ mini_saint/front/pages/index.js
@@ -2,49 +2,35 @@ import Head from 'next/head'
 import Layout from '../components/layout'
 import useSWR, { mutate } from "swr";
 import axios from "axios";
-import React, { } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/Index.module.css";
 import Navbar from "../components/navbar";
 import Card from "../components/card"; 
 import Slitbox from "../components/slitbox"
+import config from "../config/config"
 const URL = "http://localhost/api/students";
 const URL_SEL = "http://localhost/api/purchase";
 const fetcher = (key) => fetch(key).then((res) => res.json());
-const index = () => {
-  const { data, error } = useSWR(URL, fetcher, { revalidateOnFocus: false });
-  if (error) return <div>failed to load</div>;
-  if (!data) return <div>Loading...</div>;
-  console.log("data", data);
+const Index = () => {
+  // const { data, error } = useSWR(URL, fetcher, { revalidateOnFocus: false });
+  // if (error) return <div>failed to load</div>;
+  // if (!data) return <div>Loading...</div>;
+  // console.log("data", data);
 
-  const selStu = async (id) => {
-    let result = await axios.post(`${URL_SEL}/${id}`)
-    mutate(URL, data);
-  }
+  // const selStu = async (id) => {
+  //   let result = await axios.post(`${URL_SEL}/${id}`)
+  //   mutate(URL, data);
+  // }
+  const [products,setProducts] = useState([])
+    const getProducts =async()=>{
+      let allproduct = await axios.get(`${config.URL}/allproduct`)
+      setProducts(allproduct.data)
+      console.log("data>>>>", allproduct.data);
+    } 
+  useEffect(() =>{
+    getProducts()
+  },[]) 
 
-  // const showStudents = () => {
-  //   if (data.list && data.list.length) {
-  //     return data.list.map((item, index) => {
-  //       return (
-  //         <div className={styles.listItem} key={index}>
-  //           <div><b>Name:</b> {item.name}</div>
-  //           <div><b>Surname:</b> {item.surname}</div>
-  //           <div> <b>Major:</b> {item.major} </div>
-  //           <div><b>GPA:</b> {item.GPA}</div>
-
-  //           <div>
-  //             <button
-  //               className={styles.btn}
-  //               onClick={() => selStu(item.id)}
-  //             >
-  //               Select
-  //           </button></div>
-  //         </div>
-  //       );
-  //     });
-  //   } else {
-  //     return <p>Loading...</p>;
-  //   }
-  // };
   return (
     <Layout>
       <Head>
@@ -53,13 +39,23 @@ const index = () => {
       </Head>
       <Navbar />
       <div className={styles.container}>
-      {/* <Slitbox/> */}
+        <div style={{display:'flex'}} >{
+          products.map((item)=>{
+            return (<div style={{margin:5}}>
+            <Card/>
+           </div> )
+          })
+        }
+        </div>
+      </div>
+      {/* <div className={styles.container}>
+      <Slitbox/>
       <Card/>
         
-      </div>
+      </div> */}
 
   
     </Layout>
   );
 };
-export default index;
+export default Index;
